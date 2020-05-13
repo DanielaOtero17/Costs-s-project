300c03656cdb279412fe1afa58ade99bee17c505
DanielaOtero17 <jessot@hotmail.com>
2020-05-13 14:39:21 -0500

    Atribbutes from Pedido class are already.

--- Costos por órdenes de producción/Classes/Pedido.cs
+++ Costos por órdenes de producción/Classes/Pedido.cs
@@ -8,12 +8,22 @@ namespace Costos_por_órdenes_de_producción.Classes
 {
     class Pedido
     {
-        public String id { get; set; }
+        // Número de identificación del pedido
+        public int numeroPedido { get; set; }
+        // Cantidad del artículo a producir
+        public int cantidad { get; set; }
+        //Cliente que realiza el pedido
+        public Cliente cliente { get; set; }
+        //Artículo que se va a producir
+        public Articulo articulo { get; set; }
 
 
-        public Pedido(String i)
+        public Pedido(int i, Cliente cli, Articulo art, int cant)
         {
-            id = i;
+            numeroPedido = i;
+            cantidad = cant;
+            art = null;
+            cli = null;
         }
     }
 }
